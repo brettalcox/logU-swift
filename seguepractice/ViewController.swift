8f438e220dc30ff0057925a2a1f132df532f0e7d
Brett Alcox <brettalcox@gmail.com>
2016-01-07 17:02:50 -0800

    Initial Commit

---
+++ seguepractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seguepractice
+//
+//  Created by Brett on 1/7/16.
+//  Copyright © 2016 Brett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
