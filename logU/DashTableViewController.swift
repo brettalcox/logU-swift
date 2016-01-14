5d05742a7055518fae9045afa7733b16d6240128
Brett Alcox <brettalcox@gmail.com>
2016-01-13 18:06:33 -0600

    Table updates when lift is submitted

--- logU/DashTableViewController.swift
+++ logU/DashTableViewController.swift
@@ -30,6 +30,15 @@ class DashTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    override func viewDidAppear(animated: Bool) {
+        self.tableView.reloadData()
+        let (dates, lifts, weights) = dataOfLift("https://loguapp.com/swift6.php")!
+        
+        Dates = dates
+        Lifts = lifts
+        Weights = weights
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
