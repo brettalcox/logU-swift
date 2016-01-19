ec187d9256a162e6cb402978ee946937df59d11a
Brett Alcox <brettalcox@gmail.com>
2016-01-18 23:39:28 -0600

    Changed date text field to uidatepicker, and lift text field to uipickerview

--- logU/ViewController.swift
+++ logU/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate {
 
     let url_to_request:String = "https://loguapp.com/swift.php"
     let url_to_post:String = "https://loguapp.com/swift2.php"
@@ -21,9 +21,55 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var setsTextField: UITextField!
     @IBOutlet weak var repsTextField: UITextField!
     @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBAction func dateBeginEditing(sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        
+        sender.inputView = datePickerView
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "M/d/yyyy"
+        var currentSelection = dateFormatter.stringFromDate(datePickerView.date)
+        
+        dateTextField.text = currentSelection
+        dateTextField.font = UIFont(name: "System", size: 14)
+        
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+    }
+    
+    
+    var pickOptions = ["Squat", "Pause Squat", "Front Squat", "Bench", "Close Grip Bench", "Incline Bench", "Pause Bench", "Floor Press", "Deadlift", "Deficit Deadlift", "Pause Deadlift", "Snatch Grip Deadlift", "Overhead Press", "Sots Press", "Pullups", "Dips", "Bent Over Rows", "Kroc Rows", "Straight Bar Bicep Curls", "EZ Bar Bicep Curls", "Barbell Bicep Curls", "Snatch", "Clean and Jerk", "Power Clean", "Power Snatch", "Hang Clean", "Hang Snatch"]
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        let dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        dateTextField.text = convertDateFormatter(dateFormatter.stringFromDate(sender.date))
+        
+    }
 
+    func convertDateFormatter(date: String) -> String
+    {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "M/d/yyyy"
+        let date = dateFormatter.dateFromString(date)
+        
+        let timeStamp = dateFormatter.stringFromDate(date!)
+        
+        
+        return timeStamp
+    }
 
     @IBOutlet weak var logButton: UIBarButtonItem!
+    @IBAction func liftEditingStart(sender: UITextField) {
+        liftTextField.text = pickOptions[0]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +85,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         weightTextField.delegate = self
         
         logButton.enabled = false
+        
+        var pickerView = UIPickerView()
+        
+        pickerView.delegate = self
+        
+        liftTextField.inputView = pickerView
         
         addDoneButton()
         
@@ -57,6 +109,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         setsTextField.inputAccessoryView = keyboardToolbar
         repsTextField.inputAccessoryView = keyboardToolbar
         weightTextField.inputAccessoryView = keyboardToolbar
+        dateTextField.inputAccessoryView = keyboardToolbar
+        liftTextField.inputAccessoryView = keyboardToolbar
     }
 
     
@@ -126,6 +180,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let dateCheck = dateTextField.text ?? ""
         let liftCheck = liftTextField.text ?? ""
         logButton.enabled = !dateCheck.isEmpty && !liftCheck.isEmpty
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOptions.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOptions[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        liftTextField.text = pickOptions[row]
     }
 
 }
